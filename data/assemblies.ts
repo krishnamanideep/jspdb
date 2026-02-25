f2f35cc159b8dd94add11d24839afb66822c0210
Krishna Manideep <krishnamanideep@users.noreply.github.com>
2026-02-25 15:17:19 +0530

    fix(data): add actual AP assemblies mapping and secure firestore top-level tables

--- data/assemblies.ts
+++ data/assemblies.ts
@@ -1,37 +1,698 @@
-
 export const ASSEMBLIES = [
-    { id: '1', name: 'Mannadipet' },
-    { id: '2', name: 'Thirubhuvanai (SC)' },
-    { id: '3', name: 'Ossudu (SC)' },
-    { id: '4', name: 'Mangalam' },
-    { id: '5', name: 'Villianur' },
-    { id: '6', name: 'Ozhukarai' },
-    { id: '7', name: 'Kadirkamam' },
-    { id: '8', name: 'Indira Nagar' },
-    { id: '9', name: 'Thattanchavady' },
-    { id: '10', name: 'Kamaraj Nagar' },
-    { id: '11', name: 'Lawspet' },
-    { id: '12', name: 'Kalapet' },
-    { id: '13', name: 'Muthialpet' },
-    { id: '14', name: 'Raj Bhavan' },
-    { id: '15', name: 'Oupalam' },
-    { id: '16', name: 'Orleampeth' },
-    { id: '17', name: 'Nellithope' },
-    { id: '18', name: 'Mudaliarpet' },
-    { id: '19', name: 'Ariankuppam' },
-    { id: '20', name: 'Manavely' },
-    { id: '21', name: 'Embalam (SC)' },
-    { id: '22', name: 'Nettapakkam (SC)' },
-    { id: '23', name: 'Bahour' },
-    { id: '24', name: 'Nedungadu (SC)' },
-    { id: '25', name: 'Thirunallar' },
-    { id: '26', name: 'Karaikal North' },
-    { id: '27', name: 'Karaikal South' },
-    { id: '28', name: 'Neravy T R Pattinam' },
-    { id: '29', name: 'Mahe' },
-    { id: '30', name: 'Yanam' }
+    {
+        "id": "108.0",
+        "name": "AMADALAVALASA"
+    },
+    {
+        "id": "109.0",
+        "name": "ETCHERLA"
+    },
+    {
+        "id": "111.0",
+        "name": "ICHAPURAM"
+    },
+    {
+        "id": "112.0",
+        "name": "NARASANNAPETA"
+    },
+    {
+        "id": "113.0",
+        "name": "PALAKONDA"
+    },
+    {
+        "id": "114.0",
+        "name": "PATHAPATNAM"
+    },
+    {
+        "id": "116.0",
+        "name": "SRIKAKULAM"
+    },
+    {
+        "id": "117.0",
+        "name": "TEKKALI"
+    },
+    {
+        "id": "120.0",
+        "name": "CHEEPURUPALLI"
+    },
+    {
+        "id": "121.0",
+        "name": "GAJAPATHINAGARAM"
+    },
+    {
+        "id": "122.0",
+        "name": "BOBBILI"
+    },
+    {
+        "id": "124.0",
+        "name": "PARVATHIPURAM"
+    },
+    {
+        "id": "125.0",
+        "name": "SALUR"
+    },
+    {
+        "id": "127.0",
+        "name": "SRUNGAVARAPUKOTA"
+    },
+    {
+        "id": "129.0",
+        "name": "VIZIANAGARAM"
+    },
+    {
+        "id": "133.0",
+        "name": "ANAKAPALLI"
+    },
+    {
+        "id": "134.0",
+        "name": "CHODAVARAM"
+    },
+    {
+        "id": "135.0",
+        "name": "ELAMANCHILI"
+    },
+    {
+        "id": "136.0",
+        "name": "MADUGULA"
+    },
+    {
+        "id": "137.0",
+        "name": "NARSIPATNAM"
+    },
+    {
+        "id": "138.0",
+        "name": "PADERU"
+    },
+    {
+        "id": "140.0",
+        "name": "PAYAKARAOPETA"
+    },
+    {
+        "id": "141.0",
+        "name": "PENDURTHI"
+    },
+    {
+        "id": "146.0",
+        "name": "Amalapuram"
+    },
+    {
+        "id": "149.0",
+        "name": "JAGGAMPETA"
+    },
+    {
+        "id": "152.0",
+        "name": "KOTHAPETA"
+    },
+    {
+        "id": "153.0",
+        "name": "MUMMIDIVARAM"
+    },
+    {
+        "id": "155.0",
+        "name": "PEDDAPURAM"
+    },
+    {
+        "id": "156.0",
+        "name": "PITHAPURAM"
+    },
+    {
+        "id": "157.0",
+        "name": "PRATHIPAD"
+    },
+    {
+        "id": "159.0",
+        "name": "RAMACHANDRAPURAM"
+    },
+    {
+        "id": "160.0",
+        "name": "RAZOLE"
+    },
+    {
+        "id": "163.0",
+        "name": "TUNI"
+    },
+    {
+        "id": "167.0",
+        "name": "BHIMAVARAM"
+    },
+    {
+        "id": "168.0",
+        "name": "CHINTALAPUDI"
+    },
+    {
+        "id": "169.0",
+        "name": "DENDULUR"
+    },
+    {
+        "id": "170.0",
+        "name": "ELURU"
+    },
+    {
+        "id": "171.0",
+        "name": "GOPALPURAM"
+    },
+    {
+        "id": "172.0",
+        "name": "KOVVUR"
+    },
+    {
+        "id": "174.0",
+        "name": "PALACOLE"
+    },
+    {
+        "id": "176.0",
+        "name": "POLAVARAM"
+    },
+    {
+        "id": "177.0",
+        "name": "TADEPALLIGUDEM"
+    },
+    {
+        "id": "178.0",
+        "name": "TANUKU"
+    },
+    {
+        "id": "179.0",
+        "name": "UNDI"
+    },
+    {
+        "id": "180.0",
+        "name": "UNGUTUR"
+    },
+    {
+        "id": "181.0",
+        "name": "ACHANTA"
+    },
+    {
+        "id": "182.0",
+        "name": "AVANIGADDA"
+    },
+    {
+        "id": "184.0",
+        "name": "GANNAVARAM (KRISHNA)"
+    },
+    {
+        "id": "185.0",
+        "name": "GUDIVADA"
+    },
+    {
+        "id": "186.0",
+        "name": "JAGGAYYAPET"
+    },
+    {
+        "id": "187.0",
+        "name": "KAIKALUR"
+    },
+    {
+        "id": "191.0",
+        "name": "MYLAVARAM"
+    },
+    {
+        "id": "192.0",
+        "name": "NANDIGAMA"
+    },
+    {
+        "id": "193.0",
+        "name": "NUZVID"
+    },
+    {
+        "id": "194.0",
+        "name": "TIRUVURU"
+    },
+    {
+        "id": "195.0",
+        "name": "VIJAYAWADA EAST"
+    },
+    {
+        "id": "196.0",
+        "name": "VIJAYAWADA WEST"
+    },
+    {
+        "id": "199.0",
+        "name": "CHILAKALURIPET"
+    },
+    {
+        "id": "203.0",
+        "name": "GURZALA"
+    },
+    {
+        "id": "205.0",
+        "name": "MACHERLA"
+    },
+    {
+        "id": "206.0",
+        "name": "MANGALAGIRI"
+    },
+    {
+        "id": "207.0",
+        "name": "VINUKONDA"
+    },
+    {
+        "id": "208.0",
+        "name": "NARASARAOPET"
+    },
+    {
+        "id": "209.0",
+        "name": "BAPATLA"
+    },
+    {
+        "id": "210.0",
+        "name": "PEDDAKURAPADU"
+    },
+    {
+        "id": "211.0",
+        "name": "PONNUR"
+    },
+    {
+        "id": "212.0",
+        "name": "PRATHIPADU"
+    },
+    {
+        "id": "213.0",
+        "name": "REPALLE"
+    },
+    {
+        "id": "214.0",
+        "name": "SATTENAPALLI"
+    },
+    {
+        "id": "215.0",
+        "name": "TADIKONDA"
+    },
+    {
+        "id": "216.0",
+        "name": "TENALI"
+    },
+    {
+        "id": "217.0",
+        "name": "VEMURU"
+    },
+    {
+        "id": "218.0",
+        "name": "ADDANKI"
+    },
+    {
+        "id": "219.0",
+        "name": "CHIRALA"
+    },
+    {
+        "id": "221.0",
+        "name": "DARSI"
+    },
+    {
+        "id": "222.0",
+        "name": "GIDDALUR"
+    },
+    {
+        "id": "223.0",
+        "name": "KANDUKUR"
+    },
+    {
+        "id": "224.0",
+        "name": "KANIGIRI"
+    },
+    {
+        "id": "225.0",
+        "name": "KONDEPI"
+    },
+    {
+        "id": "226.0",
+        "name": "MARKAPUR"
+    },
+    {
+        "id": "227.0",
+        "name": "ONGOLE"
+    },
+    {
+        "id": "228.0",
+        "name": "PARCHUR"
+    },
+    {
+        "id": "229.0",
+        "name": "SANTHANUTHALAPADU"
+    },
+    {
+        "id": "231.0",
+        "name": "GUDUR"
+    },
+    {
+        "id": "232.0",
+        "name": "KAVALI"
+    },
+    {
+        "id": "233.0",
+        "name": "KOVUR"
+    },
+    {
+        "id": "236.0",
+        "name": "SARVEPALLI"
+    },
+    {
+        "id": "237.0",
+        "name": "SULLURPET"
+    },
+    {
+        "id": "238.0",
+        "name": "UDAYAGIRI"
+    },
+    {
+        "id": "239.0",
+        "name": "VENKATAGIRI"
+    },
+    {
+        "id": "241.0",
+        "name": "ATMAKUR"
+    },
+    {
+        "id": "242.0",
+        "name": "BADVEL"
+    },
+    {
+        "id": "243.0",
+        "name": "KADAPA"
+    },
+    {
+        "id": "244.0",
+        "name": "JAMMALAMADUGU"
+    },
+    {
+        "id": "245.0",
+        "name": "KAMALAPURAM"
+    },
+    {
+        "id": "246.0",
+        "name": "KODUR"
+    },
+    {
+        "id": "248.0",
+        "name": "RAYACHOTY"
+    },
+    {
+        "id": "249.0",
+        "name": "MYDUKUR"
+    },
+    {
+        "id": "250.0",
+        "name": "PRODDATUR"
+    },
+    {
+        "id": "251.0",
+        "name": "PULIVENDLA"
+    },
+    {
+        "id": "252.0",
+        "name": "RAJAMPET"
+    },
+    {
+        "id": "253.0",
+        "name": "ADONI"
+    },
+    {
+        "id": "254.0",
+        "name": "ALLAGADDA"
+    },
+    {
+        "id": "255.0",
+        "name": "ALUR"
+    },
+    {
+        "id": "257.0",
+        "name": "DHONE"
+    },
+    {
+        "id": "258.0",
+        "name": "KODUMUR"
+    },
+    {
+        "id": "260.0",
+        "name": "KURNOOL"
+    },
+    {
+        "id": "261.0",
+        "name": "NANDIKOTKUR"
+    },
+    {
+        "id": "262.0",
+        "name": "NANDYAL"
+    },
+    {
+        "id": "263.0",
+        "name": "PANYAM"
+    },
+    {
+        "id": "264.0",
+        "name": "PATTIKONDA"
+    },
+    {
+        "id": "265.0",
+        "name": "YEMMIGANUR"
+    },
+    {
+        "id": "267.0",
+        "name": "DHARMAVARAM"
+    },
+    {
+        "id": "270.0",
+        "name": "HINDUPUR"
+    },
+    {
+        "id": "271.0",
+        "name": "KADIRI"
+    },
+    {
+        "id": "272.0",
+        "name": "KALYANDURG"
+    },
+    {
+        "id": "273.0",
+        "name": "MADAKASIRA"
+    },
+    {
+        "id": "275.0",
+        "name": "PENUKONDA"
+    },
+    {
+        "id": "276.0",
+        "name": "RAYADURG"
+    },
+    {
+        "id": "277.0",
+        "name": "SINGANAMALA"
+    },
+    {
+        "id": "278.0",
+        "name": "TADPATRI"
+    },
+    {
+        "id": "279.0",
+        "name": "URAVAKONDA"
+    },
+    {
+        "id": "280.0",
+        "name": "CHANDRAGIRI"
+    },
+    {
+        "id": "281.0",
+        "name": "CHITTOOR"
+    },
+    {
+        "id": "282.0",
+        "name": "KUPPAM"
+    },
+    {
+        "id": "283.0",
+        "name": "NAGARI"
+    },
+    {
+        "id": "284.0",
+        "name": "PALAMANER"
+    },
+    {
+        "id": "285.0",
+        "name": "PILERU"
+    },
+    {
+        "id": "286.0",
+        "name": "PUNGANUR"
+    },
+    {
+        "id": "288.0",
+        "name": "SATYAVEDU"
+    },
+    {
+        "id": "289.0",
+        "name": "SRIKALAHASTI"
+    },
+    {
+        "id": "290.0",
+        "name": "THAMBALLAPALLE"
+    },
+    {
+        "id": "291.0",
+        "name": "TIRUPATI"
+    },
+    {
+        "id": "294.0",
+        "name": "MADANPALLE"
+    },
+    {
+        "id": "297.0",
+        "name": "GUNTAKAL"
+    },
+    {
+        "id": "298.0",
+        "name": "ANANTAPUR URBAN"
+    },
+    {
+        "id": "299.0",
+        "name": "RAPTADU"
+    },
+    {
+        "id": "300.0",
+        "name": "PUTTAPARTHI"
+    },
+    {
+        "id": "301.0",
+        "name": "GANGADHARA NELLORE"
+    },
+    {
+        "id": "302.0",
+        "name": "PUTHALAPATTU"
+    },
+    {
+        "id": "303.0",
+        "name": "RAJANAGARAM"
+    },
+    {
+        "id": "304.0",
+        "name": "RAJAHMUNDRY CITY"
+    },
+    {
+        "id": "305.0",
+        "name": "RAJAHMUNDRY RURAL"
+    },
+    {
+        "id": "306.0",
+        "name": "RAMPACHODAVARAM"
+    },
+    {
+        "id": "307.0",
+        "name": "KAKINADA RURAL"
+    },
+    {
+        "id": "308.0",
+        "name": "KAKINADA CITY"
+    },
+    {
+        "id": "309.0",
+        "name": "MANDAPETA"
+    },
+    {
+        "id": "310.0",
+        "name": "GANNAVARAM (EG)"
+    },
+    {
+        "id": "311.0",
+        "name": "GUNTUR EAST"
+    },
+    {
+        "id": "312.0",
+        "name": "GUNTUR WEST"
+    },
+    {
+        "id": "327.0",
+        "name": "PEDANA"
+    },
+    {
+        "id": "328.0",
+        "name": "MACHILIPATNAM"
+    },
+    {
+        "id": "329.0",
+        "name": "PAMARRU"
+    },
+    {
+        "id": "330.0",
+        "name": "PENAMALURU"
+    },
+    {
+        "id": "331.0",
+        "name": "VIJAYAWADA CENTRAL"
+    },
+    {
+        "id": "332.0",
+        "name": "SRISAILAM"
+    },
+    {
+        "id": "333.0",
+        "name": "BANAGANAPALLE"
+    },
+    {
+        "id": "334.0",
+        "name": "MANTRALAYAM"
+    },
+    {
+        "id": "340.0",
+        "name": "NELLORE CITY"
+    },
+    {
+        "id": "341.0",
+        "name": "NELLORE RURAL"
+    },
+    {
+        "id": "344.0",
+        "name": "YERRAGONDAPALEM"
+    },
+    {
+        "id": "352.0",
+        "name": "PALASA"
+    },
+    {
+        "id": "353.0",
+        "name": "RAJAM"
+    },
+    {
+        "id": "354.0",
+        "name": "VISAKHAPATNAM EAST"
+    },
+    {
+        "id": "355.0",
+        "name": "VISAKHAPATNAM SOUTH"
+    },
+    {
+        "id": "356.0",
+        "name": "VISAKHAPATNAM NORTH"
+    },
+    {
+        "id": "357.0",
+        "name": "VISAKHAPATNAM WEST"
+    },
+    {
+        "id": "358.0",
+        "name": "GAJUWAKA"
+    },
+    {
+        "id": "359.0",
+        "name": "ARAKU VALLEY"
+    },
+    {
+        "id": "360.0",
+        "name": "KURUPAM"
+    },
+    {
+        "id": "361.0",
+        "name": "NELLIMARLA"
+    },
+    {
+        "id": "366.0",
+        "name": "NIDADAVOLE"
+    },
+    {
+        "id": "368.0",
+        "name": "BHIMILI"
+    }
 ];
 
 export const getAssemblyName = (id: string) => {
-    return ASSEMBLIES.find(a => a.id === id)?.name || `Assembly ${id}`;
+    return ASSEMBLIES.find(a => a.id === id)?.name || \Assembly \;
 };
