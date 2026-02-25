91eb635fe2c7f8ae1e81d848a882e6035be36517
Krishna Manideep <krishnamanideep@users.noreply.github.com>
2026-02-25 15:54:21 +0530

    fix(data): remove decimal from assembly ids and allow survey data reads

--- data/assemblies.ts
+++ data/assemblies.ts
@@ -1,698 +1,698 @@
 export const ASSEMBLIES = [
     {
-        "id": "108.0",
+        "id": "108",
         "name": "AMADALAVALASA"
     },
     {
-        "id": "109.0",
+        "id": "109",
         "name": "ETCHERLA"
     },
     {
-        "id": "111.0",
+        "id": "111",
         "name": "ICHAPURAM"
     },
     {
-        "id": "112.0",
+        "id": "112",
         "name": "NARASANNAPETA"
     },
     {
-        "id": "113.0",
+        "id": "113",
         "name": "PALAKONDA"
     },
     {
-        "id": "114.0",
+        "id": "114",
         "name": "PATHAPATNAM"
     },
     {
-        "id": "116.0",
+        "id": "116",
         "name": "SRIKAKULAM"
     },
     {
-        "id": "117.0",
+        "id": "117",
         "name": "TEKKALI"
     },
     {
-        "id": "120.0",
+        "id": "120",
         "name": "CHEEPURUPALLI"
     },
     {
-        "id": "121.0",
+        "id": "121",
         "name": "GAJAPATHINAGARAM"
     },
     {
-        "id": "122.0",
+        "id": "122",
         "name": "BOBBILI"
     },
     {
-        "id": "124.0",
+        "id": "124",
         "name": "PARVATHIPURAM"
     },
     {
-        "id": "125.0",
+        "id": "125",
         "name": "SALUR"
     },
     {
-        "id": "127.0",
+        "id": "127",
         "name": "SRUNGAVARAPUKOTA"
     },
     {
-        "id": "129.0",
+        "id": "129",
         "name": "VIZIANAGARAM"
     },
     {
-        "id": "133.0",
+        "id": "133",
         "name": "ANAKAPALLI"
     },
     {
-        "id": "134.0",
+        "id": "134",
         "name": "CHODAVARAM"
     },
     {
-        "id": "135.0",
+        "id": "135",
         "name": "ELAMANCHILI"
     },
     {
-        "id": "136.0",
+        "id": "136",
         "name": "MADUGULA"
     },
     {
-        "id": "137.0",
+        "id": "137",
         "name": "NARSIPATNAM"
     },
     {
-        "id": "138.0",
+        "id": "138",
         "name": "PADERU"
     },
     {
-        "id": "140.0",
+        "id": "140",
         "name": "PAYAKARAOPETA"
     },
     {
-        "id": "141.0",
+        "id": "141",
         "name": "PENDURTHI"
     },
     {
-        "id": "146.0",
+        "id": "146",
         "name": "Amalapuram"
     },
     {
-        "id": "149.0",
+        "id": "149",
         "name": "JAGGAMPETA"
     },
     {
-        "id": "152.0",
+        "id": "152",
         "name": "KOTHAPETA"
     },
     {
-        "id": "153.0",
+        "id": "153",
         "name": "MUMMIDIVARAM"
     },
     {
-        "id": "155.0",
+        "id": "155",
         "name": "PEDDAPURAM"
     },
     {
-        "id": "156.0",
+        "id": "156",
         "name": "PITHAPURAM"
     },
     {
-        "id": "157.0",
+        "id": "157",
         "name": "PRATHIPAD"
     },
     {
-        "id": "159.0",
+        "id": "159",
         "name": "RAMACHANDRAPURAM"
     },
     {
-        "id": "160.0",
+        "id": "160",
         "name": "RAZOLE"
     },
     {
-        "id": "163.0",
+        "id": "163",
         "name": "TUNI"
     },
     {
-        "id": "167.0",
+        "id": "167",
         "name": "BHIMAVARAM"
     },
     {
-        "id": "168.0",
+        "id": "168",
         "name": "CHINTALAPUDI"
     },
     {
-        "id": "169.0",
+        "id": "169",
         "name": "DENDULUR"
     },
     {
-        "id": "170.0",
+        "id": "170",
         "name": "ELURU"
     },
     {
-        "id": "171.0",
+        "id": "171",
         "name": "GOPALPURAM"
     },
     {
-        "id": "172.0",
+        "id": "172",
         "name": "KOVVUR"
     },
     {
-        "id": "174.0",
+        "id": "174",
         "name": "PALACOLE"
     },
     {
-        "id": "176.0",
+        "id": "176",
         "name": "POLAVARAM"
     },
     {
-        "id": "177.0",
+        "id": "177",
         "name": "TADEPALLIGUDEM"
     },
     {
-        "id": "178.0",
+        "id": "178",
         "name": "TANUKU"
     },
     {
-        "id": "179.0",
+        "id": "179",
         "name": "UNDI"
     },
     {
-        "id": "180.0",
+        "id": "180",
         "name": "UNGUTUR"
     },
     {
-        "id": "181.0",
+        "id": "181",
         "name": "ACHANTA"
     },
     {
-        "id": "182.0",
+        "id": "182",
         "name": "AVANIGADDA"
     },
     {
-        "id": "184.0",
+        "id": "184",
         "name": "GANNAVARAM (KRISHNA)"
     },
     {
-        "id": "185.0",
+        "id": "185",
         "name": "GUDIVADA"
     },
     {
-        "id": "186.0",
+        "id": "186",
         "name": "JAGGAYYAPET"
     },
     {
-        "id": "187.0",
+        "id": "187",
         "name": "KAIKALUR"
     },
     {
-        "id": "191.0",
+        "id": "191",
         "name": "MYLAVARAM"
     },
     {
-        "id": "192.0",
+        "id": "192",
         "name": "NANDIGAMA"
     },
     {
-        "id": "193.0",
+        "id": "193",
         "name": "NUZVID"
     },
     {
-        "id": "194.0",
+        "id": "194",
         "name": "TIRUVURU"
     },
     {
-        "id": "195.0",
+        "id": "195",
         "name": "VIJAYAWADA EAST"
     },
     {
-        "id": "196.0",
+        "id": "196",
         "name": "VIJAYAWADA WEST"
     },
     {
-        "id": "199.0",
+        "id": "199",
         "name": "CHILAKALURIPET"
     },
     {
-        "id": "203.0",
+        "id": "203",
         "name": "GURZALA"
     },
     {
-        "id": "205.0",
+        "id": "205",
         "name": "MACHERLA"
     },
     {
-        "id": "206.0",
+        "id": "206",
         "name": "MANGALAGIRI"
     },
     {
-        "id": "207.0",
+        "id": "207",
         "name": "VINUKONDA"
     },
     {
-        "id": "208.0",
+        "id": "208",
         "name": "NARASARAOPET"
     },
     {
-        "id": "209.0",
+        "id": "209",
         "name": "BAPATLA"
     },
     {
-        "id": "210.0",
+        "id": "210",
         "name": "PEDDAKURAPADU"
     },
     {
-        "id": "211.0",
+        "id": "211",
         "name": "PONNUR"
     },
     {
-        "id": "212.0",
+        "id": "212",
         "name": "PRATHIPADU"
     },
     {
-        "id": "213.0",
+        "id": "213",
         "name": "REPALLE"
     },
     {
-        "id": "214.0",
+        "id": "214",
         "name": "SATTENAPALLI"
     },
     {
-        "id": "215.0",
+        "id": "215",
         "name": "TADIKONDA"
     },
     {
-        "id": "216.0",
+        "id": "216",
         "name": "TENALI"
     },
     {
-        "id": "217.0",
+        "id": "217",
         "name": "VEMURU"
     },
     {
-        "id": "218.0",
+        "id": "218",
         "name": "ADDANKI"
     },
     {
-        "id": "219.0",
+        "id": "219",
         "name": "CHIRALA"
     },
     {
-        "id": "221.0",
+        "id": "221",
         "name": "DARSI"
     },
     {
-        "id": "222.0",
+        "id": "222",
         "name": "GIDDALUR"
     },
     {
-        "id": "223.0",
+        "id": "223",
         "name": "KANDUKUR"
     },
     {
-        "id": "224.0",
+        "id": "224",
         "name": "KANIGIRI"
     },
     {
-        "id": "225.0",
+        "id": "225",
         "name": "KONDEPI"
     },
     {
-        "id": "226.0",
+        "id": "226",
         "name": "MARKAPUR"
     },
     {
-        "id": "227.0",
+        "id": "227",
         "name": "ONGOLE"
     },
     {
-        "id": "228.0",
+        "id": "228",
         "name": "PARCHUR"
     },
     {
-        "id": "229.0",
+        "id": "229",
         "name": "SANTHANUTHALAPADU"
     },
     {
-        "id": "231.0",
+        "id": "231",
         "name": "GUDUR"
     },
     {
-        "id": "232.0",
+        "id": "232",
         "name": "KAVALI"
     },
     {
-        "id": "233.0",
+        "id": "233",
         "name": "KOVUR"
     },
     {
-        "id": "236.0",
+        "id": "236",
         "name": "SARVEPALLI"
     },
     {
-        "id": "237.0",
+        "id": "237",
         "name": "SULLURPET"
     },
     {
-        "id": "238.0",
+        "id": "238",
         "name": "UDAYAGIRI"
     },
     {
-        "id": "239.0",
+        "id": "239",
         "name": "VENKATAGIRI"
     },
     {
-        "id": "241.0",
+        "id": "241",
         "name": "ATMAKUR"
     },
     {
-        "id": "242.0",
+        "id": "242",
         "name": "BADVEL"
     },
     {
-        "id": "243.0",
+        "id": "243",
         "name": "KADAPA"
     },
     {
-        "id": "244.0",
+        "id": "244",
         "name": "JAMMALAMADUGU"
     },
     {
-        "id": "245.0",
+        "id": "245",
         "name": "KAMALAPURAM"
     },
     {
-        "id": "246.0",
+        "id": "246",
         "name": "KODUR"
     },
     {
-        "id": "248.0",
+        "id": "248",
         "name": "RAYACHOTY"
     },
     {
-        "id": "249.0",
+        "id": "249",
         "name": "MYDUKUR"
     },
     {
-        "id": "250.0",
+        "id": "250",
         "name": "PRODDATUR"
     },
     {
-        "id": "251.0",
+        "id": "251",
         "name": "PULIVENDLA"
     },
     {
-        "id": "252.0",
+        "id": "252",
         "name": "RAJAMPET"
     },
     {
-        "id": "253.0",
+        "id": "253",
         "name": "ADONI"
     },
     {
-        "id": "254.0",
+        "id": "254",
         "name": "ALLAGADDA"
     },
     {
-        "id": "255.0",
+        "id": "255",
         "name": "ALUR"
     },
     {
-        "id": "257.0",
+        "id": "257",
         "name": "DHONE"
     },
     {
-        "id": "258.0",
+        "id": "258",
         "name": "KODUMUR"
     },
     {
-        "id": "260.0",
+        "id": "260",
         "name": "KURNOOL"
     },
     {
-        "id": "261.0",
+        "id": "261",
         "name": "NANDIKOTKUR"
     },
     {
-        "id": "262.0",
+        "id": "262",
         "name": "NANDYAL"
     },
     {
-        "id": "263.0",
+        "id": "263",
         "name": "PANYAM"
     },
     {
-        "id": "264.0",
+        "id": "264",
         "name": "PATTIKONDA"
     },
     {
-        "id": "265.0",
+        "id": "265",
         "name": "YEMMIGANUR"
     },
     {
-        "id": "267.0",
+        "id": "267",
         "name": "DHARMAVARAM"
     },
     {
-        "id": "270.0",
+        "id": "270",
         "name": "HINDUPUR"
     },
     {
-        "id": "271.0",
+        "id": "271",
         "name": "KADIRI"
     },
     {
-        "id": "272.0",
+        "id": "272",
         "name": "KALYANDURG"
     },
     {
-        "id": "273.0",
+        "id": "273",
         "name": "MADAKASIRA"
     },
     {
-        "id": "275.0",
+        "id": "275",
         "name": "PENUKONDA"
     },
     {
-        "id": "276.0",
+        "id": "276",
         "name": "RAYADURG"
     },
     {
-        "id": "277.0",
+        "id": "277",
         "name": "SINGANAMALA"
     },
     {
-        "id": "278.0",
+        "id": "278",
         "name": "TADPATRI"
     },
     {
-        "id": "279.0",
+        "id": "279",
         "name": "URAVAKONDA"
     },
     {
-        "id": "280.0",
+        "id": "280",
         "name": "CHANDRAGIRI"
     },
     {
-        "id": "281.0",
+        "id": "281",
         "name": "CHITTOOR"
     },
     {
-        "id": "282.0",
+        "id": "282",
         "name": "KUPPAM"
     },
     {
-        "id": "283.0",
+        "id": "283",
         "name": "NAGARI"
     },
     {
-        "id": "284.0",
+        "id": "284",
         "name": "PALAMANER"
     },
     {
-        "id": "285.0",
+        "id": "285",
         "name": "PILERU"
     },
     {
-        "id": "286.0",
+        "id": "286",
         "name": "PUNGANUR"
     },
     {
-        "id": "288.0",
+        "id": "288",
         "name": "SATYAVEDU"
     },
     {
-        "id": "289.0",
+        "id": "289",
         "name": "SRIKALAHASTI"
     },
     {
-        "id": "290.0",
+        "id": "290",
         "name": "THAMBALLAPALLE"
     },
     {
-        "id": "291.0",
+        "id": "291",
         "name": "TIRUPATI"
     },
     {
-        "id": "294.0",
+        "id": "294",
         "name": "MADANPALLE"
     },
     {
-        "id": "297.0",
+        "id": "297",
         "name": "GUNTAKAL"
     },
     {
-        "id": "298.0",
+        "id": "298",
         "name": "ANANTAPUR URBAN"
     },
     {
-        "id": "299.0",
+        "id": "299",
         "name": "RAPTADU"
     },
     {
-        "id": "300.0",
+        "id": "300",
         "name": "PUTTAPARTHI"
     },
     {
-        "id": "301.0",
+        "id": "301",
         "name": "GANGADHARA NELLORE"
     },
     {
-        "id": "302.0",
+        "id": "302",
         "name": "PUTHALAPATTU"
     },
     {
-        "id": "303.0",
+        "id": "303",
         "name": "RAJANAGARAM"
     },
     {
-        "id": "304.0",
+        "id": "304",
         "name": "RAJAHMUNDRY CITY"
     },
     {
-        "id": "305.0",
+        "id": "305",
         "name": "RAJAHMUNDRY RURAL"
     },
     {
-        "id": "306.0",
+        "id": "306",
         "name": "RAMPACHODAVARAM"
     },
     {
-        "id": "307.0",
+        "id": "307",
         "name": "KAKINADA RURAL"
     },
     {
-        "id": "308.0",
+        "id": "308",
         "name": "KAKINADA CITY"
     },
     {
-        "id": "309.0",
+        "id": "309",
         "name": "MANDAPETA"
     },
     {
-        "id": "310.0",
+        "id": "310",
         "name": "GANNAVARAM (EG)"
     },
     {
-        "id": "311.0",
+        "id": "311",
         "name": "GUNTUR EAST"
     },
     {
-        "id": "312.0",
+        "id": "312",
         "name": "GUNTUR WEST"
     },
     {
-        "id": "327.0",
+        "id": "327",
         "name": "PEDANA"
     },
     {
-        "id": "328.0",
+        "id": "328",
         "name": "MACHILIPATNAM"
     },
     {
-        "id": "329.0",
+        "id": "329",
         "name": "PAMARRU"
     },
     {
-        "id": "330.0",
+        "id": "330",
         "name": "PENAMALURU"
     },
     {
-        "id": "331.0",
+        "id": "331",
         "name": "VIJAYAWADA CENTRAL"
     },
     {
-        "id": "332.0",
+        "id": "332",
         "name": "SRISAILAM"
     },
     {
-        "id": "333.0",
+        "id": "333",
         "name": "BANAGANAPALLE"
     },
     {
-        "id": "334.0",
+        "id": "334",
         "name": "MANTRALAYAM"
     },
     {
-        "id": "340.0",
+        "id": "340",
         "name": "NELLORE CITY"
     },
     {
-        "id": "341.0",
+        "id": "341",
         "name": "NELLORE RURAL"
     },
     {
-        "id": "344.0",
+        "id": "344",
         "name": "YERRAGONDAPALEM"
     },
     {
-        "id": "352.0",
+        "id": "352",
         "name": "PALASA"
     },
     {
-        "id": "353.0",
+        "id": "353",
         "name": "RAJAM"
     },
     {
-        "id": "354.0",
+        "id": "354",
         "name": "VISAKHAPATNAM EAST"
     },
     {
-        "id": "355.0",
+        "id": "355",
         "name": "VISAKHAPATNAM SOUTH"
     },
     {
-        "id": "356.0",
+        "id": "356",
         "name": "VISAKHAPATNAM NORTH"
     },
     {
-        "id": "357.0",
+        "id": "357",
         "name": "VISAKHAPATNAM WEST"
     },
     {
-        "id": "358.0",
+        "id": "358",
         "name": "GAJUWAKA"
     },
     {
-        "id": "359.0",
+        "id": "359",
         "name": "ARAKU VALLEY"
     },
     {
-        "id": "360.0",
+        "id": "360",
         "name": "KURUPAM"
     },
     {
-        "id": "361.0",
+        "id": "361",
         "name": "NELLIMARLA"
     },
     {
-        "id": "366.0",
+        "id": "366",
         "name": "NIDADAVOLE"
     },
     {
-        "id": "368.0",
+        "id": "368",
         "name": "BHIMILI"
     }
 ];
 
 export const getAssemblyName = (id: string) => {
-    return ASSEMBLIES.find(a => a.id === id)?.name || \Assembly \;
+    return ASSEMBLIES.find(a => a.id === id)?.name || Assembly \;
 };
